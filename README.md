63a20283526cb4a64727744cf45ff07e79356cb9
bhargav-vasist <bhargavclick@gmail.com>
2023-12-12 04:34:20 +0530

    Adds dashboard

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # visitor-management-poc
+
+# Missing features
+- Image Picker with Camera
+- Autocomplete for Past Visitors
+- Offline storage of Visitor data
+- Alternative authentication methods
+
+# Inefficiencies
+- Firestore DB references leaking across multiple files
+- Dashboard Count is not real-time
+- Dashboard Count is slow and manually aggregates instead of using Firestore functions
+
+# Things that can be better
+- Loading States and Refresh Indicators
+- Better error messages and handling
+- Jest tests for counting logic, auth, DB read/write calls
+- Reusing phone number components with logic
+- Modular components for text/buttons so we could add animation/themes
--- visitor-mgmt/app/(home)/home.js
+++ visitor-mgmt/app/(home)/home.js
@@ -1,19 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
-// Import components for lists, charts etc.
+import React, { useState, useEffect } from 'react';
+import countVisits from "../../services/network/visits";
+import { Text, Card } from '@ui-kitten/components';
+import { View, StyleSheet, ScrollView } from 'react-native';
 
 const HomeDashboard = () => {
-    const [visitorData, setVisitorData] = useState([]);
+    const [visitorCount, setVisitorCount] = useState({ day: 0, week: 0, month: 0 });
 
     useEffect(() => {
-        // Fetch and subscribe to visitor data
+        const fetchVisitorCounts = async () => {
+            const counts = await countVisits();
+            setVisitorCount(counts);
+        };
+
+        fetchVisitorCounts();
     }, []);
 
     return (
-        <View>
-            {/* Render your visitor data here */}
-        </View>
+        <ScrollView style={styles.container}>
+            <Card style={styles.card}>
+                <Text category="h5" style={styles.cardTitle}>Visitors Today</Text>
+                <Text category="h1" style={styles.cardNumber}>{visitorCount.day}</Text>
+            </Card>
+            <Card style={styles.card}>
+                <Text category="h5" style={styles.cardTitle}>Visitors This Week</Text>
+                <Text category="h1" style={styles.cardNumber}>{visitorCount.week}</Text>
+            </Card>
+            <Card style={styles.card}>
+                <Text category="h5" style={styles.cardTitle}>Visitors This Month</Text>
+                <Text category="h1" style={styles.cardNumber}>{visitorCount.month}</Text>
+            </Card>
+            {/* Render list of visitors or charts here */}
+        </ScrollView>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 10,
+    },
+    card: {
+        margin: 10,
+        padding: 20,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    cardTitle: {
+        textAlign: 'center',
+        marginBottom: 5,
+    },
+    cardNumber: {
+        textAlign: 'center',
+        fontSize: 36, // Adjust the font size as needed
+    }
+});
 
 export default HomeDashboard;
